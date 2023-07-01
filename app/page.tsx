c4d59cec4c57b55d8212e60c5ff22f6a8de88d29
Hamed Ghazali <hamed.ghazali@digikala.com>
2023-07-01 15:56:50 +0330

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,10 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main
+      className="flex min-h-screen flex-col items-center p-24"
+      style={{ background: "white" }}
+    >
       {isLoading ? (
         <Spin size="large" />
       ) : (
